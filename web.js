76d802bc494fb761a07ceef7a92b3f7a4da24f40
Vijayasree Panchapakesan <vijayasree.panch@gmail.com>
2013-07-09 12:45:17 +0000

    Updated to read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var buffer = fs.readFileSync('index.html');
+var outString = buffer.toString('utf8',0,buffer.length);
+console.log(outString);
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+  response.send(outString);
 });
 
 var port = process.env.PORT || 5000;
